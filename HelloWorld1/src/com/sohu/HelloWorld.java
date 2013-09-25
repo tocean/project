c26dc2515aaa678823f9294b500f203a6b0b48f5
yuxiangyang <yuxiangyang@.sohu-inc.com>
2013-09-25 15:15:16 +0800

    init helloword

---
+++ HelloWorld1/src/com/sohu/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sohu;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+		System.out.println("hello, world");
+		System.out.println("hwllo, world-1");
+		System.out.println("hello, world-2");
+		System.out.println("hello, world-3");
+		System.out.println("hello, world-4");
+		System.out.println("hello, world-5");
+		System.out.println("hello, world-6");
+	}
+}
